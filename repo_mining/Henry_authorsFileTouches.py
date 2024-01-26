e837ba6b64e49dc4de48772aaf9fbbe1942ca023
kyomi <87290470+koyomi7@users.noreply.github.com>
2024-01-25 21:38:18 -0800

    lab1

--- repo_mining/Henry_authorsFileTouches.py
+++ repo_mining/Henry_authorsFileTouches.py
@@ -1,11 +1,10 @@
 import json
 import requests
 import csv
-
 import os
 
 if not os.path.exists("data"):
- os.makedirs("data")
+    os.makedirs("data")
 
 # GitHub Authentication function
 def github_auth(url, lsttoken, ct):
@@ -29,7 +28,7 @@ def countfiles(dictfiles, lsttokens, repo):
     ct = 0  # token counter
 
     try:
-        # loop though all the commit pages until the last returned empty page
+        # loop through all the commit pages until the last returned empty page
         while True:
             spage = str(ipage)
             commitsUrl = 'https://api.github.com/repos/' + repo + '/commits?page=' + spage + '&per_page=100'
@@ -38,53 +37,59 @@ def countfiles(dictfiles, lsttokens, repo):
             # break out of the while loop if there are no more commits in the pages
             if len(jsonCommits) == 0:
                 break
-            # iterate through the list of commits in  spage
+            # iterate through the list of commits in spage
             for shaObject in jsonCommits:
                 sha = shaObject['sha']
                 # For each commit, use the GitHub commit API to extract the files touched by the commit
                 shaUrl = 'https://api.github.com/repos/' + repo + '/commits/' + sha
                 shaDetails, ct = github_auth(shaUrl, lsttokens, ct)
+
+                # Extracting original author and last modified date information
+                author = shaDetails['commit']['author']['name']
+                date = shaDetails['commit']['author']['date']
+
                 filesjson = shaDetails['files']
                 for filenameObj in filesjson:
                     filename = filenameObj['filename']
-                    dictfiles[filename] = dictfiles.get(filename, 0) + 1
-                    print(filename)
+                    # Store filename, original author, and last modified date information in the dictionary
+                    dictfiles[filename] = {
+                        'OriginalAuthor': author,
+                        'LastModifiedDate': date
+                    }
+
+                    print(f"Filename: {filename}, Original Author: {author}, Last Modified Date: {date}")
             ipage += 1
     except:
         print("Error receiving data")
         exit(0)
+
 # GitHub repo
 repo = 'scottyab/rootbeer'
 # repo = 'Skyscanner/backpack' # This repo is commit heavy. It takes long to finish executing
 # repo = 'k9mail/k-9' # This repo is commit heavy. It takes long to finish executing
 # repo = 'mendhak/gpslogger'
 
-
 # put your tokens here
 # Remember to empty the list when going to commit to GitHub.
-# Otherwise they will all be reverted and you will have to re-create them
-# I would advise to create more than one token for repos with heavy commits
-lstTokens = ["fake token"]
+# Otherwise, they will all be reverted, and you will have to re-create them
+# I would advise creating more than one token for repos with heavy commits
+lstTokens = [
+    "fake"
+]
 
 dictfiles = dict()
 countfiles(dictfiles, lstTokens, repo)
 print('Total number of files: ' + str(len(dictfiles)))
 
 file = repo.split('/')[1]
-# change this to the path of your file
-fileOutput = 'C:/Users/11984/Documents/GitHub/2DRogueLikeUnityGame/repo_mining' + file + '.csv'
-rows = ["Filename", "Touches"]
+fileOutput = 'C:/Users/11984/Documents/GitHub/2DRogueLikeUnityGame/repo_mining' + file + '_authors.csv'
+rows = ["Filename", "OriginalAuthor", "LastModifiedDate"]
 fileCSV = open(fileOutput, 'w')
 writer = csv.writer(fileCSV)
 writer.writerow(rows)
 
-bigcount = None
-bigfilename = None
-for filename, count in dictfiles.items():
-    rows = [filename, count]
+for filename, details in dictfiles.items():
+    rows = [filename, details['OriginalAuthor'], details['LastModifiedDate']]
     writer.writerow(rows)
-    if bigcount is None or count > bigcount:
-        bigcount = count
-        bigfilename = filename
+
 fileCSV.close()
-print('The file ' + bigfilename + ' has been touched ' + str(bigcount) + ' times.')
