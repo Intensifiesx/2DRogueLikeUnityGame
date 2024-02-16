6c25c137326a92d0f31981b0072f29f731513d56
Koi <kelvindo2002@gmail.com>
2024-02-15 16:43:44 -0800

    chatgpt lab

---
+++ chatgpt/Kelvin-Do_fizzbuzz.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+int main()
+{
+    int n = 0;
+    std::cout << "Enter your number ";
+    std::cin >> n;    
+
+    for(int i = 1; i <= n; i++)
+    {
+        if(i % 15 == 0)
+            std::cout << "FizzBuzz\n";
+        else if(i % 3 == 0)  
+            std::cout << "Fizz\n";
+        else if(i % 5 == 0)
+            std::cout << "Buzz\n";
+        else
+            std::cout << i << "\n";       
+    }
+}
