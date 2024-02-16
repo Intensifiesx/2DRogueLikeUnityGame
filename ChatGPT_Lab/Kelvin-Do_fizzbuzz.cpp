e6e018f7dac7bf5eea8d7ca521da77a0ec7a4a4b
Koi <kelvindo2002@gmail.com>
2024-02-15 17:11:46 -0800

    ChatGPT optimization

--- ChatGPT_Lab/Kelvin-Do_fizzbuzz.cpp
+++ ChatGPT_Lab/Kelvin-Do_fizzbuzz.cpp
@@ -8,9 +8,10 @@ int main()
 
     for(int i = 1; i <= n; i++)
     {
-        if(i % 15 == 0)
-            std::cout << "FizzBuzz\n";
-        else if(i % 3 == 0)  
+        // ChatGPT commented out the two lines below
+        // if(i % 15 == 0)
+        //     std::cout << "FizzBuzz\n";
+        if(i % 3 == 0)  // changed this to an if statement, rather than a else if statement
             std::cout << "Fizz\n";
         else if(i % 5 == 0)
             std::cout << "Buzz\n";
