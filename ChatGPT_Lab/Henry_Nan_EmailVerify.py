863a05c8a2527f771275ba95b51dff8c06c1d4ae
SwiftNemesis <bbsowers@gmail.com>
2024-02-22 22:04:52 -0800

    Delete unnecessary files and code

--- ChatGPT_Lab/Henry_Nan_EmailVerify.py
+++
@@ -1,7 +0,0 @@
-import re # Import the regular expression module
-
-def validate_email(email):
-    # Regular expression pattern for basic email validation
-    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
-    return bool(re.match(pattern, email))
-# The regular expression pattern is a string that represents the pattern of the email address.
