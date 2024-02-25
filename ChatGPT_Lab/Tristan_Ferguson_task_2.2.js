21f3606b72612ced3bdff5be96a352bbe1b279c2
fergut3 <128553171+fergut3@users.noreply.github.com>
2024-02-25 11:17:21 -0800

    Create Tristan_Ferguson_task_2.2.js

---
+++ ChatGPT_Lab/Tristan_Ferguson_task_2.2.js
@@ -0,0 +1,21 @@
+class Car {
+  constructor(brand) {
+    this.carname = brand;
+  }
+  present() {
+    return 'I have a ' + this.carname;
+  }
+}
+
+class Model extends Car {
+  constructor(brand, mod) {
+    super(brand);
+    this.model = mod;
+  }
+  show() {
+    return this.present() + ', it is a ' + this.model;
+  }
+}
+
+let myCar = new Model("Ford", "Mustang");
+document.getElementById("demo").innerHTML = myCar.show();
